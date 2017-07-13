bd110061cf3c487125ee4c25efb29f6159f7cb81
Misha <mackan@gapps.ispu.ru>
2017-07-13 18:15:50 +0300

    Add all files

---
+++ Runge/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Runge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.y1st = new System.Windows.Forms.TextBox();
+            this.y2st = new System.Windows.Forms.TextBox();
+            this.y3st = new System.Windows.Forms.TextBox();
+            this.rtb = new System.Windows.Forms.RichTextBox();
+            this.Parallelcb = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Tftb = new System.Windows.Forms.TextBox();
+            this.CalcBtn = new System.Windows.Forms.Button();
+            this.pgb = new System.Windows.Forms.ProgressBar();
+            this.cancelbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "y1(0)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "y2(0)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "y3(0)";
+            // 
+            // y1st
+            // 
+            this.y1st.Location = new System.Drawing.Point(48, 10);
+            this.y1st.Name = "y1st";
+            this.y1st.Size = new System.Drawing.Size(39, 20);
+            this.y1st.TabIndex = 3;
+            this.y1st.Text = "1";
+            // 
+            // y2st
+            // 
+            this.y2st.Location = new System.Drawing.Point(48, 36);
+            this.y2st.Name = "y2st";
+            this.y2st.Size = new System.Drawing.Size(39, 20);
+            this.y2st.TabIndex = 4;
+            this.y2st.Text = "1";
+            // 
+            // y3st
+            // 
+            this.y3st.Location = new System.Drawing.Point(48, 62);
+            this.y3st.Name = "y3st";
+            this.y3st.Size = new System.Drawing.Size(39, 20);
+            this.y3st.TabIndex = 5;
+            this.y3st.Text = "0";
+            // 
+            // rtb
+            // 
+            this.rtb.Location = new System.Drawing.Point(12, 119);
+            this.rtb.Name = "rtb";
+            this.rtb.Size = new System.Drawing.Size(178, 165);
+            this.rtb.TabIndex = 6;
+            this.rtb.Text = "";
+            // 
+            // Parallelcb
+            // 
+            this.Parallelcb.AutoSize = true;
+            this.Parallelcb.Checked = true;
+            this.Parallelcb.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.Parallelcb.Location = new System.Drawing.Point(130, 13);
+            this.Parallelcb.Name = "Parallelcb";
+            this.Parallelcb.Size = new System.Drawing.Size(60, 17);
+            this.Parallelcb.TabIndex = 7;
+            this.Parallelcb.Text = "Parallel";
+            this.Parallelcb.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Time";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(322, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Время окончания";
+            // 
+            // Tftb
+            // 
+            this.Tftb.Location = new System.Drawing.Point(424, 10);
+            this.Tftb.Name = "Tftb";
+            this.Tftb.Size = new System.Drawing.Size(29, 20);
+            this.Tftb.TabIndex = 10;
+            this.Tftb.Text = "60";
+            // 
+            // CalcBtn
+            // 
+            this.CalcBtn.Location = new System.Drawing.Point(378, 251);
+            this.CalcBtn.Name = "CalcBtn";
+            this.CalcBtn.Size = new System.Drawing.Size(75, 23);
+            this.CalcBtn.TabIndex = 11;
+            this.CalcBtn.Text = "Calculate";
+            this.CalcBtn.UseVisualStyleBackColor = true;
+            this.CalcBtn.Click += new System.EventHandler(this.CalcBtn_Click);
+            // 
+            // pgb
+            // 
+            this.pgb.Location = new System.Drawing.Point(196, 241);
+            this.pgb.Name = "pgb";
+            this.pgb.Size = new System.Drawing.Size(160, 33);
+            this.pgb.TabIndex = 12;
+            // 
+            // cancelbtn
+            // 
+            this.cancelbtn.Location = new System.Drawing.Point(378, 222);
+            this.cancelbtn.Name = "cancelbtn";
+            this.cancelbtn.Size = new System.Drawing.Size(75, 23);
+            this.cancelbtn.TabIndex = 13;
+            this.cancelbtn.Text = "Cancel";
+            this.cancelbtn.UseVisualStyleBackColor = true;
+            this.cancelbtn.Click += new System.EventHandler(this.cancelbtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 296);
+            this.Controls.Add(this.cancelbtn);
+            this.Controls.Add(this.pgb);
+            this.Controls.Add(this.CalcBtn);
+            this.Controls.Add(this.Tftb);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Parallelcb);
+            this.Controls.Add(this.rtb);
+            this.Controls.Add(this.y3st);
+            this.Controls.Add(this.y2st);
+            this.Controls.Add(this.y1st);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox y1st;
+        private System.Windows.Forms.TextBox y2st;
+        private System.Windows.Forms.TextBox y3st;
+        private System.Windows.Forms.RichTextBox rtb;
+        private System.Windows.Forms.CheckBox Parallelcb;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Tftb;
+        private System.Windows.Forms.Button CalcBtn;
+        private System.Windows.Forms.ProgressBar pgb;
+        private System.Windows.Forms.Button cancelbtn;
+    }
+}
+
---
+++ Runge/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+namespace Runge
+{
+    public partial class Form1 : Form
+    {
+        double F1(double[] y, double x)
+        {
+            return -(55 + y[2]) * y[0] + 65 * y[1];
+        }
+
+        double F2(double[] y, double x)
+        {
+            return 0.0785 * (y[0] - y[1]);
+        }
+
+        double F3(double[] y, double x)
+        {
+            return 0.1 * y[0];
+        }
+
+        double F(double[] y, double x, int i)
+        {
+            switch (i)
+            {
+                case 0:
+                    return F1(y, x);
+                 
+                case 1:
+                    return F2(y, x);
+                   
+                case 2:
+                    return F3(y, x);
+                default:
+                    return 0;
+            }
+        }
+   
+        private CancellationTokenSource _cts;
+        const int c = 3;
+        const double KOEF = 0.166666667;
+        const double TAU = 0.001;
+        double Tst = 0, Tfn;
+        double t;
+        double x = 0;
+        double[] y,ans;
+        double[] R1, R2, R3, R4, Help1, Help2, Help3;
+
+        private void CalcBtn_Click(object sender, EventArgs e)
+        {
+            Calc();
+        }
+
+        private void cancelbtn_Click(object sender, EventArgs e)
+        {
+            if (_cts == null) return;
+            else _cts.Cancel();
+        }
+
+        private async void Calc()
+        {
+            R1 = new double[3];
+            R2 = new double[3];
+            R3 = new double[3];
+            R4 = new double[3];
+            Help1 = new double[3];
+            Help2 = new double[3];
+            Help3 = new double[3];
+            Progress<int> prog = new Progress<int>();
+            prog.ProgressChanged += prog_ProgressChanged;
+            y = new double[] { Convert.ToDouble(y1st.Text), Convert.ToDouble(y2st.Text), Convert.ToDouble(y3st.Text)};
+            t = Tst;
+            Tfn = Convert.ToDouble(Tftb.Text);
+            Stopwatch time = new Stopwatch();
+            time.Start();
+            if (Parallelcb.Checked)
+            {
+                _cts = new CancellationTokenSource();
+                try
+                {
+                    ans = await RungeCuttaFourParallel(y, Tst, Tfn, _cts.Token, prog);
+                    time.Stop();
+                    rtb.AppendText("y1:" + ans[0] + "; " + "y2:" + ans[1] + "; " + "y3:" + ans[2] + ";" + Environment.NewLine + "Time:" + ((double)time.ElapsedMilliseconds / 1000).ToString() + Environment.NewLine);
+
+                }
+                catch (OperationCanceledException)
+                {
+                    time.Stop();
+                    MessageBox.Show("Операция была отменена");
+                }
+                catch
+                {
+                    time.Stop();
+                    MessageBox.Show("Ошибка!");
+                }
+
+            }
+            else
+            {
+                ans = RungeCuttaFour(Tst, Tfn, y);
+                time.Stop();
+                rtb.AppendText("y1:" + ans[0] + "; " + "y2:" + ans[1] + "; " + "y3:" + ans[2] + ";" + Environment.NewLine + "Time:" + ((double)time.ElapsedMilliseconds / 1000).ToString() + Environment.NewLine);
+            }
+        }
+        private Task<double[]> RungeCuttaFourParallel(double[] y, double Tst, double Tfn,CancellationToken token, IProgress<int> prog)
+        {
+            Task<double[]> t = new Task<double[]>(() => {
+                double[] ans = new double[y.Length];
+                Array.Copy(y, ans, y.Length);
+                int pr =0;
+                var time = Tst;
+                while (time < Tfn)
+                {
+                    for (int i = 0; i < c; i++)
+                    {
+                        R1[i] = TAU * F(ans, x, i);
+                        Help1[i] = ans[i] + 0.5 * R1[i];
+                    }
+                    for (int i = 0; i < c; i++)
+                    {
+                        R2[i] = TAU * F(Help1, x, i);
+                        Help2[i] = ans[i] + 0.5 * R2[i];
+                    }
+                    for (int i = 0; i < c; i++)
+                    {
+                        R3[i] = TAU * F(Help2, x, i);
+                        Help3[i] = ans[i] + R3[i];
+                    }
+                    for (int i = 0; i < c; i++)
+                    {
+                        R4[i] = TAU * F(Help3, x, i);
+                    }
+                    Parallel.For(0, c, new ParallelOptions { CancellationToken = token }, (i, state) =>
+                       {
+                           //
+                          // ((int)(time / Tfn)) * 100
+                           ans[i] = ans[i] + KOEF * (R1[i] + 2 * R2[i] + 2 * R3[i] + R4[i]);
+                           token.ThrowIfCancellationRequested();
+                          // Interlocked.Increment(ref count);
+                       });
+                    pr = (int)((time / Tfn) * 100 +1);
+                    prog.Report(pr);
+
+                    time += TAU;
+                }
+                return ans;
+            }, token);
+            t.Start();
+            return t;
+        }
+        private double[] RungeCuttaFour(double Tst,double Tfn, double[] y)
+        {
+            double[] res = new double[y.Length];
+            Array.Copy(y, res, y.Length);
+            var time = Tst;
+            while (time < Tfn)
+            {
+                for (int i = 0; i < c; i++)
+                {
+                    R1[i] = TAU * F(res, x, i);
+                    Help1[i] = res[i] + 0.5 * R1[i];
+                }
+                for (int i = 0; i < c; i++)
+                {
+                    R2[i] = TAU * F(Help1, x, i);
+                    Help2[i] = res[i] + 0.5 * R2[i];
+                }
+                for (int i = 0; i < c; i++)
+                {
+                    R3[i] = TAU * F(Help2, x, i);
+                    Help3[i] = res[i] + R3[i];
+                }
+                for (int i = 0; i < c; i++)
+                {
+                    R4[i] = TAU * F(Help3, x, i);
+                }
+                for (int i = 0; i < c; i++)
+                {
+                    res[i] = res[i] + KOEF * (R1[i] + 2 * R2[i] + 2 * R3[i] + R4[i]);
+                }
+                time += TAU;
+            }
+            return res;
+        }
+        void prog_ProgressChanged(object sender, int e)
+        {
+            pgb.Value = e;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
+
+
+/*      Task<double[]> tsl =  RungeCuttaFourParallel(y, Tst, Tfn, _cts.Token, prog);
+                tsl.ContinueWith((origin) => 
+                {
+                    if (origin.IsCanceled)
+                    {
+                        time.Stop();
+                        MessageBox.Show("Операция была отменена");
+                    }
+                    if (origin.IsFaulted)
+                    {
+                        origin.Exception.Handle((ex) => {
+                            time.Stop();
+                            MessageBox.Show(ex.Message);
+                            return true;
+                        });
+                    }
+                    if(origin.IsCompleted)
+                    {
+                        time.Stop();
+                        rtb.AppendText("y1:" + y[0] + "; " + "y2:" + y[1] + "; " + "y3:" + y[2] + "; Time:" + ((double)time.ElapsedMilliseconds / 1000).ToString() + Environment.NewLine);
+                    }
+                },TaskScheduler.FromCurrentSynchronizationContext());*/
